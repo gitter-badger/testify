5f72619a2485e7dc5e2b8f93cbb590b2b5dbcc17
Sharmarke Aden <sharmarke.aden@tangocard.com>
2015-11-22 18:39:51 -0800

    trying to get release to work

--- sonatype.sh
+++ sonatype.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
-if [ "$TRAVIS_PULL_REQUEST" = "true" ]; then
-  echo "Pull Request on $TRAVIS_BRANCH' branch. Artifacts not deployed."
-  exit 0
-fi
+if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
+    if [ "$TRAVIS_BRANCH" = "master" ]; then
+      echo "Releasing $TRAVIS_BRANCH' branch."
+      mvn nexus-staging-maven-plugin:release --settings settings.xml -Prelease -Dmaven.test.skip -B -T 1C
 
-if [ "$TRAVIS_BRANCH" = "master" ]; then
-  echo "Releasing $TRAVIS_BRANCH' branch."
-  mvn nexus-staging-maven-plugin:release --settings settings.xml -Prelease -Dmaven.test.skip -B -T 1C
+    elif [ "$TRAVIS_BRANCH" = "develop" ]; then
+      echo "Staging '$TRAVIS_BRANCH' branch"
+      mvn clean deploy --settings settings.xml -Prelease -Dmaven.test.skip -B -T 1C
 
-elif [ "$TRAVIS_BRANCH" = "develop" ]; then
-  echo "Staging '$TRAVIS_BRANCH' branch"
-  mvn clean deploy --settings settings.xml -Prelease -Dmaven.test.skip -B -T 1C
-
-else
-    echo "Unknown '$TRAVIS_BRANCH' branch. Artifacts not deployed."
+    else
+        echo "Unknown '$TRAVIS_BRANCH' branch. Artifacts not deployed."
+    fi
 fi
