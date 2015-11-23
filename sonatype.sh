ae1d7fdf0fe11d52be83a3c0362ff28230fb2aa6
Sharmarke Aden <sharmarke.aden@tangocard.com>
2015-11-23 07:02:15 -0800

    leaned up sonatype script

--- sonatype.sh
+++ sonatype.sh
@@ -18,11 +18,11 @@
 if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
     if [ "$TRAVIS_BRANCH" = "master" ]; then
       echo "Releasing $TRAVIS_BRANCH' branch."
-      mvn nexus-staging-maven-plugin:release --settings settings.xml -Prelease -Dmaven.test.skip -B -T 1C
+      mvn nexus-staging:release --settings settings.xml -B
 
     elif [ "$TRAVIS_BRANCH" = "develop" ]; then
       echo "Staging '$TRAVIS_BRANCH' branch"
-      mvn clean deploy --settings settings.xml -Prelease -Dmaven.test.skip -B -T 1C
+      mvn clean deploy --settings settings.xml -Prelease -B
 
     else
         echo "Unknown '$TRAVIS_BRANCH' branch. Artifacts not deployed."
