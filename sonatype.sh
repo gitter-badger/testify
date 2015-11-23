5395a38b38147fabf9209a914c170efbb0b105a2
Sharmarke Aden <sharmarke.aden@tangocard.com>
2015-11-22 18:12:09 -0800

    added sonatype repos trying to see if maven parallel works in travis env

--- sonatype.sh
+++ sonatype.sh
@@ -2,11 +2,11 @@
 
 if [ "$TRAVIS_BRANCH" = "master" ]; then
   echo "Releasing $TRAVIS_BRANCH' branch."
-  mvn nexus-staging:release --settings settings.xml -Prelease -Dmaven.test.skip -B
+  mvn nexus-staging-maven-plugin:release --settings settings.xml -Prelease -Dmaven.test.skip -B -T 1C
 
 elif [ "$TRAVIS_BRANCH" = "develop" ]; then
   echo "Staging '$TRAVIS_BRANCH' branch"
-  mvn clean deploy --settings settings.xml -Prelease -Dmaven.test.skip -B
+  mvn clean deploy --settings settings.xml -Prelease -Dmaven.test.skip -B -T 1C
 
 else
     echo "Unknown '$TRAVIS_BRANCH' branch. Artifacts not deployed."
